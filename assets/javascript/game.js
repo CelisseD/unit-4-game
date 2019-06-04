9d83f534bbc294e2443d352376e2dfdb26a1c57a
CelisseD <43502934+CelisseD@users.noreply.github.com>
2019-06-03 23:54:11 -0400

    added code that allos user to click a crystal to receive a random value added to the previous value, in order to reach the target number

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -4,7 +4,7 @@
 // The player will be shown a random number (targetNumber) at the start of the game
 // When the player clicks on a crystal, it will add a specific amount of points to the player's total score.
 // The game will hide this amount until the player clicks a crystal
-// When they do click a crustal, update the player's score counter (scoreCounter)
+// When they do click a crystal, update the player's score counter (scoreCounter)
 // The player wins if their total score matches the random number from the beginning of the game
 // The player loses if their score goes above the random number.
 // The game restarts whenever the player wins or loses.
@@ -14,13 +14,21 @@
 var targetNumber; // score the player should reach by clicking the crystals
 var gameLost;
 var gameWin;
+var previousNumber = 0;
 
+
+
+// Assigns a target number that the user should reach
+targetNumber = Math.floor(Math.random() * 102) + 19;
+console.log(targetNumber);
+
+// Adds the target number to the HTML
+$("#target").html("Target Number: " + targetNumber);
 
 // for loop to add 4 divs to the HTML
-
 for (var i = 0; i < 4; i++) {
 
-    var randomNumber = Math.floor(Math.random() * 12);
+    var randomNumber = Math.floor(Math.random() * 11) + 1;
     console.log(randomNumber);
     // generates four random number values upon page load.
     var crystal = $("<div>");
@@ -29,6 +37,22 @@ for (var i = 0; i < 4; i++) {
             "dataRandom": randomNumber
             // assigns a random number value to each div
         });
+            crystal.html(randomNumber);
 
     $(".crystals").append(crystal);
+
 }
+
+// Allows user to click on a crystal and receive a number value which has pulled from dataRandom
+$(".crystal").on("click", function() {
+    var result;
+    var num = parseInt($(this).attr('dataRandom'));
+    previousNumber += num;
+    if (previousNumber > targetNumber) {
+        console.log("You Lose!");
+    }
+    else if (previousNumber === targetNumber){
+        console.log("You Win!");
+    }
+    console.log(previousNumber)
+});
